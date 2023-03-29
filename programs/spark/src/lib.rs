3d2c62cec90b0d8cd4e2291d4d6344de9eafe85b
kratos2377 <shobityadav23@gmail.com>
2023-03-29 22:18:17 +0530

    Tweet struct and impl added

--- programs/spark/src/lib.rs
+++ programs/spark/src/lib.rs
@@ -13,3 +13,33 @@ pub mod spark {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct SendATweet<'info> {
+    pub my_tweet: Account<'info , TweetOnSolana>,
+    pub sender_of_tweet: Signer<'info>,
+    pub system_program: Program<'info , System>
+}
+
+
+#[account]
+pub struct TweetOnSolana {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const PUBLIC_TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 8;
+const MAX_TOPIC_LENGTH: usize = 50 * 4;
+const MAX_CONTENT_LENGTH: usize = 280 * 4;
+
+
+impl TweetOnSolana {
+    const LEN: usize = DISCRIMINATOR_LENGTH * PUBLIC_KEY_LENGTH * PUBLIC_TIMESTAMP_LENGTH
+                        * STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH
+                        * STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH;
+}
